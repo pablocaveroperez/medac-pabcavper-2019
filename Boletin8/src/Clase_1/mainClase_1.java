357154cb4bfb1ad02363697e33b62239c9738929
pabcavper <gelves.mula@gmail.com>
2019-12-18 13:56:37 +0100

    ejercicio clase 1

--- Boletin8/src/Clase_1/mainClase_1.java
+++ Boletin8/src/Clase_1/mainClase_1.java
@@ -2,6 +2,13 @@ package Clase_1;
 
 public class mainClase_1 {
     public static void main(String[] args) {
+        Producto vProductos[] = new Producto[3];
+        vProductos[0] = new Producto("Cola-Cao", 2.4f,48);
+        vProductos[1] = new Producto("Agua", 0.8f,200);
+        vProductos[2] = new Producto("Zumito", 1.3f,20);
 
+        for (int i = 0; i < vProductos.length; i++){
+            System.out.println(vProductos[i]);
+        }
     }
 }
