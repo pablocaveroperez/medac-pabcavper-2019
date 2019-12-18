f8937680ed248e5a2221f1001eb4788447c7c8ff
pabcavper <gelves.mula@gmail.com>
2019-12-18 13:52:18 +0100

    ejercicio clase 1

---
+++ Boletin8/src/Clase_1/Producto.java
@@ -0,0 +1,61 @@
+package Clase_1;
+public class Producto {
+    private String nombre;
+    private float precio;
+    private int unidades;
+
+    private final int PRECIO_MAXIMO = 1000000;
+
+    public Producto(String nombre, float precio, int unidades) {
+        this.setNombre(nombre);
+        this.setPrecio(precio);
+        this.setUnidades(unidades);
+    }
+
+    public float valorEnStock() {
+        return unidades * precio;
+    }
+
+    public void incrementarStock(int incremento) {
+        unidades = unidades + incremento;
+    }
+
+    public void disminuirStock(int decremento) {
+        unidades = unidades - decremento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUnidades(int unidades) {
+        if(unidades > 0) {
+            this.unidades = unidades;
+        }
+    }
+
+    public void setPrecio(float precio) {
+        if (precio > 0 && precio < PRECIO_MAXIMO)
+            this.precio = precio;
+    }
+
+    public String toString() {
+        String sResultado = "";
+        sResultado += "Nombre: " + this.getNombre() + "\n";
+        sResultado += "Precio: " + this.getPrecio() + "\n";
+        sResultado += "Unidades: " + this.getUnidades() + "\n";
+        return sResultado;
+    }
+}
