c10c8af1cab06ade9ac3210f76aa30dc97b5061d
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-23 20:21:24 +0100

    Problema 1 (Fecha)

--- Boletin8/src/Problema1/Fecha.java
+++ Boletin8/src/Problema1/Fecha.java
@@ -33,10 +33,26 @@ public class Fecha {
         this.shAnio = bAnio;
     }
 
+    public byte diasDelMes(byte bMes, short shAnio){
+        byte diasDelMes;
+        if (bMes == 1 || bMes == 3 || bMes == 5 || bMes == 7 || bMes == 8 || bMes == 10 || bMes == 12)
+            diasDelMes = 31;
+        else if (bMes == 4 || bMes == 6 || bMes == 9 || bMes == 11)
+            diasDelMes = 30;
+        else
+            if (esBisiesto(shAnio))
+                diasDelMes = 29;
+            else
+                diasDelMes = 28;
+
+        return diasDelMes;
+    }
+
     public boolean esFechaValida(byte bDia, byte bMes, short shAnio){
         boolean esValida = true;
-        if (getbMes() == 2){
-            if (esBisiesto(getbAnio())){
+        if (bMes == 2){
+            if (esBisiesto(shAnio)){
+                if (bDia >= 1 && bDia <= diasDelMes(bMes,shAnio))
 
             }
         }
