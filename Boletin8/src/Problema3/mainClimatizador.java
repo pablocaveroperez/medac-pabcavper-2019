96616677635be780fb4af8917e67809bd93d42b6
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-27 12:07:24 +0100

    Problema 3 (Climatizador)

--- Boletin8/src/Problema3/mainClimatizador.java
+++ Boletin8/src/Problema3/mainClimatizador.java
@@ -4,5 +4,9 @@ public class mainClimatizador {
     public static void main(String[] args) {
         Climatizador climatizador = new Climatizador();
 
+        climatizador.setTemperaturaDeseada(25);
+        climatizador.setTemperaturaActual(14);
+
+        climatizador.activar();
     }
 }
