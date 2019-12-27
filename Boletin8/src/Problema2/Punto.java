d198614a6d4db6f6543f7e82eada9b1079ad0304
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-27 10:51:02 +0100

    Problema 2 (Punto)

---
+++ Boletin8/src/Problema2/Punto.java
@@ -0,0 +1,37 @@
+package Problema2;
+
+public class Punto {
+    private double x;
+    private double y;
+
+    public Punto(){
+        setX(0);
+        setY(0);
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void borrar(){
+        setY(0);
+        setX(0);
+    }
+
+    public void mover(double x, double y){
+        setX(x);
+        setY(y);
+    }
+}
