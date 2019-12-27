0f87196fb04b8addc50868e6e13b33d8a6788d48
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-27 11:02:35 +0100

    Problema 2 (Punto)

---
+++ Boletin8/src/Problema2/mainPunto.java
@@ -0,0 +1,13 @@
+package Problema2;
+
+public class mainPunto {
+    public static void main(String[] args) {
+        Punto punto1 = new Punto();
+        Punto punto2 = new Punto();
+
+        punto1.setX(5.3);
+        punto1.setY(2);
+
+        System.out.println(punto1);
+    }
+}
