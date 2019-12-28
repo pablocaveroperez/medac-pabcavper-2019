1281acabc94609e525cdd203902d92b61e8b0c66
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-28 11:36:26 +0100

    Problema 4 (Maquina Expendedora)

--- Boletin8/src/Problema4/MaquinaExpendedora.java
+++ Boletin8/src/Problema4/MaquinaExpendedora.java
@@ -1,4 +1,64 @@
 package Problema4;
 
 public class MaquinaExpendedora {
+    private int iNumeroSerie;
+    private int iCreditoAcumulado;
+    private int iCreditoConsumido;
+    private int iDineroAcumulado;
+
+    public MaquinaExpendedora(int iNumeroSerie){
+        setiNumeroSerie(iNumeroSerie);
+    }
+
+    public int getiNumeroSerie() {
+        return iNumeroSerie;
+    }
+
+    public void setiNumeroSerie(int iNumeroSerie) {
+        this.iNumeroSerie = iNumeroSerie;
+    }
+
+    public int getiCreditoAcumulado() {
+        return iCreditoAcumulado;
+    }
+
+    public void setiCreditoAcumulado(int iCreditoAcumulado) {
+        this.iCreditoAcumulado = iCreditoAcumulado;
+    }
+
+    public int getiCreditoConsumido() {
+        return iCreditoConsumido;
+    }
+
+    public void setiCreditoConsumido(int iCreditoConsumido) {
+        this.iCreditoConsumido = iCreditoConsumido;
+    }
+
+    public int getiDineroAcumulado() {
+        return iDineroAcumulado;
+    }
+
+    public void setiDineroAcumulado(int iDineroAcumulado) {
+        this.iDineroAcumulado = iDineroAcumulado;
+    }
+
+    public void insertarMoneda(byte valorMoneda){
+        setiCreditoAcumulado(getiCreditoAcumulado() + valorMoneda);
+    }
+
+    public void pedirProducto(byte costeProducto){
+        if (costeProducto <= getiCreditoAcumulado()) {
+            setiCreditoConsumido(getiCreditoConsumido() + costeProducto);
+            System.out.println("Has pedido el producto que cuesta "+costeProducto);
+        }else
+            System.out.println("El coste del producto es mayor que el credito acumulado. Inserta mas monedas o pide otro producto");
+    }
+
+    public int devolucionCreditoRestante(){
+        int creditoRestante = 0;
+
+        return creditoRestante;
+    }
+
+
 }
