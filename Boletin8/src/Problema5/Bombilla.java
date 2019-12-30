b96778982d8400defc5fd5ef8e42b3f32c419eb9
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-30 11:14:25 +0100

    Problema 5 (Bombilla)

---
+++ Boletin8/src/Problema5/Bombilla.java
@@ -0,0 +1,37 @@
+package Problema5;
+
+public class Bombilla {
+    private boolean bolEncendida;
+
+    public void encender(){
+        this.setBolEncendida(true);
+    }
+
+    public void apagar(){
+        this.setBolEncendida(false);
+    }
+
+    public void cambiar(){
+        if (this.bolEncendida)
+            this.setBolEncendida(false);
+        else
+            this.setBolEncendida(true);
+    }
+
+    public boolean isBolEncendida() {
+        return bolEncendida;
+    }
+
+    public void setBolEncendida(boolean bolEncendida) {
+        this.bolEncendida = bolEncendida;
+    }
+
+    public String mostrar(){
+        String salida = "";
+        if (isBolEncendida())
+            salida += '*';
+        else
+            salida += '·';
+        return salida;
+    }
+}
