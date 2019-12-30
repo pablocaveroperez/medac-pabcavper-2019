f20f7f6255f7a0dbbde00e9974c8028875e66d57
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-30 11:17:11 +0100

    Problema 5 (Bombilla)

---
+++ Boletin8/src/Problema5/Programador.java
@@ -0,0 +1,19 @@
+package Problema5;
+
+public class Programador {
+    private Bombilla bombilla1;
+    private Bombilla bombilla2;
+    private Bombilla bombilla3;
+    private Bombilla bombilla4;
+    private Bombilla bombilla5;
+    private Bombilla bombilla6;
+
+    public Programador(){
+        this.bombilla1 = new Bombilla();
+        this.bombilla2 = new Bombilla();
+        this.bombilla3 = new Bombilla();
+        this.bombilla4 = new Bombilla();
+        this.bombilla5 = new Bombilla();
+        this.bombilla6 = new Bombilla();
+    }
+}
