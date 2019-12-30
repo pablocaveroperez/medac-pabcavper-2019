efd19846ee270416b1ccd7ebcfb3269270fd4638
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-30 12:16:00 +0100

    Problema 5 (Bombilla)

--- Boletin8/src/Problema6_7_8/Coche.java
+++ Boletin8/src/Problema6_7_8/Coche.java
@@ -13,4 +13,16 @@ public class Coche {
     private final String FAMILIAR = "FAMILIAR";
     private final String DEPORTIVO = "DEPORTIVO";
 
+    public Coche(String modelo, String color, boolean bolPinturaMetalizada, String matricula, short anioFabricacion, boolean bolTodoRiesgo) {
+        this.modelo = modelo;
+        this.color = color;
+        this.bolPinturaMetalizada = bolPinturaMetalizada;
+        this.matricula = matricula;
+        this.anioFabricacion = anioFabricacion;
+        this.bolTodoRiesgo = bolTodoRiesgo;
+    }
+
+    public Coche(){
+
+    }
 }
