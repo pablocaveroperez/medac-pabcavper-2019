b99a3e26f5cfb0e3e2d91cc320545356f1bba917
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2019-12-30 12:14:47 +0100

    Problema 5 (Bombilla)

---
+++ Boletin8/src/Problema6_7_8/Coche.java
@@ -0,0 +1,16 @@
+package Problema6_7_8;
+
+public class Coche {
+    private String modelo;
+    private String color;
+    private boolean bolPinturaMetalizada;
+    private String matricula;
+    private short anioFabricacion;
+    private boolean bolTodoRiesgo;
+
+    private final String MINI = "MINI";
+    private final String UTILITARIO = "UTILITARIO";
+    private final String FAMILIAR = "FAMILIAR";
+    private final String DEPORTIVO = "DEPORTIVO";
+
+}
