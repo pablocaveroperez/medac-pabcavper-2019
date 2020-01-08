1ec9c914591dcc84da714351e13638b018dc5e57
pabcavper <gelves.mula@gmail.com>
2020-01-08 12:41:18 +0100

    Boletin 9 ejercicio clase 3

--- Boletin9/src/EjercicioClase3/Calculadora.java
+++ Boletin9/src/EjercicioClase3/Calculadora.java
@@ -1,4 +1,22 @@
 package EjercicioClase3;
 
 public class Calculadora {
+    private double oNumero;
+    //private
+
+
+    public Object getoNumero(byte bTipo) {
+        Object oNumero = this.oNumero;
+        if (bTipo == 1){
+            return Integer.parseInt((String) oNumero);
+        }else if (bTipo == 2){
+            return Long.parseLong((String) oNumero);
+        }else if (bTipo == 3){
+            return Float.parseFloat((String) oNumero);
+        }else if (bTipo == 4){
+            return oNumero;
+        }
+
+        return oNumero;
+    }
 }
