38a5bb697bb981459078f4d78699b3a6e2abaf94
pabcavper <gelves.mula@gmail.com>
2020-01-08 11:46:37 +0100

    Boletin 9 ejercicio clase 2

--- Boletin9/src/EjercicioClase2/mainPersona.java
+++ Boletin9/src/EjercicioClase2/mainPersona.java
@@ -1,4 +1,10 @@
 package EjercicioClase2;
 
 public class mainPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Antonio","Jimenez","Lopez");
+         if (!persona1.setbEdad((short) 152)){
+             System.out.println("La edad introducida es incorrecta");
+         }
+    }
 }
