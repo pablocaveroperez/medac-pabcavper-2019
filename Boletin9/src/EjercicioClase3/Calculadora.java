7800c636f24aaeb1d48f73a1ca326c2b78cde21d
pabcavper <gelves.mula@gmail.com>
2020-01-08 12:42:45 +0100

    Boletin 9 ejercicio clase 3

--- Boletin9/src/EjercicioClase3/Calculadora.java
+++ Boletin9/src/EjercicioClase3/Calculadora.java
@@ -1,20 +1,54 @@
 package EjercicioClase3;
 
 public class Calculadora {
-    private double oNumero;
-    //private
+    private double dNumero;
+    private int iNumero;
+    private long lNumero;
+    private float fNumero;
+    private Object oNumero;
 
+    public double getdNumero() {
+        return dNumero;
+    }
+
+    public void setdNumero(double dNumero) {
+        this.dNumero = dNumero;
+    }
+
+    public int getiNumero() {
+        return iNumero;
+    }
+
+    public void setiNumero(int iNumero) {
+        this.iNumero = iNumero;
+    }
+
+    public long getlNumero() {
+        return lNumero;
+    }
+
+    public void setlNumero(long lNumero) {
+        this.lNumero = lNumero;
+    }
+
+    public float getfNumero() {
+        return fNumero;
+    }
+
+    public void setfNumero(float fNumero) {
+        this.fNumero = fNumero;
+    }
 
     public Object getoNumero(byte bTipo) {
-        Object oNumero = this.oNumero;
+
         if (bTipo == 1){
-            return Integer.parseInt((String) oNumero);
+
         }else if (bTipo == 2){
-            return Long.parseLong((String) oNumero);
+
         }else if (bTipo == 3){
-            return Float.parseFloat((String) oNumero);
+
         }else if (bTipo == 4){
-            return oNumero;
+
         }
 
         return oNumero;
