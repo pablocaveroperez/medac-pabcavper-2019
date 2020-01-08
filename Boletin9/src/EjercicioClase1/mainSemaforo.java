55e81f253268fd64be537a94e57d1bd987a3ae41
pabcavper <gelves.mula@gmail.com>
2020-01-08 09:42:58 +0100

    Boletin 9 ejercicio clase 1

--- Boletin9/src/EjercicioClase1/mainSemaforo.java
+++ Boletin9/src/EjercicioClase1/mainSemaforo.java
@@ -1,4 +1,37 @@
 package EjercicioClase1;
 
 public class mainSemaforo {
+    public static void main(String []args){
+        // Paso 1
+        Semaforo oSem1 = new Semaforo();
+        Semaforo oSem2;
+        System.out.println(oSem1.imprimir());
+        // Paso 2
+        oSem1.setiColor(5);
+        System.out.println(oSem1.imprimir());
+        // Paso 3
+        oSem1.setiColor(3);
+        System.out.println(oSem1.imprimir());
+        // Paso 4
+        oSem1.setbParpadeando(true);
+        System.out.println(oSem1.imprimir());
+        // Paso 5
+        oSem1.setiColor(2);
+        System.out.println(oSem1.imprimir());
+        // Paso 6
+        oSem1.setbParpadeando(true);
+        System.out.println(oSem1.imprimir());
+        // Paso 7
+        for(int i=0; i < 5; i++) {
+            oSem1.cambia();
+            System.out.println(oSem1.imprimir());
+        }
+
+        // Paso 8
+        System.out.println("SEMAFORO 2: ");
+        oSem2 = new Semaforo();
+        oSem2.setiColor(oSem1.getiColor());
+        oSem2.setbParpadeando(oSem2.isbParpadeando());
+        System.out.println(oSem2.imprimir());
+    }
 }
