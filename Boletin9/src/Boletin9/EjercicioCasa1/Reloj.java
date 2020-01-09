310c0477a314b9bf1725bf0ee9283da7f043440f
pabcavper <gelves.mula@gmail.com>
2020-01-09 09:08:50 +0100

    Boletin 9 Reloj

--- Boletin9/src/Boletin9/EjercicioCasa1/Reloj.java
+++ Boletin9/src/Boletin9/EjercicioCasa1/Reloj.java
@@ -5,8 +5,31 @@ public class Reloj {
     private byte bMinutos;
     private byte bSegundos;
 
+    public Reloj(){
 
+    }
 
+    public void setbHoras(byte bHoras) {
+        this.bHoras = bHoras;
+    }
 
+    public void setbMinutos(byte bMinutos) {
+        this.bMinutos = bMinutos;
+    }
 
+    public void setbSegundos(byte bSegundos) {
+        this.bSegundos = bSegundos;
+    }
+
+    public byte getbHoras() {
+        return bHoras;
+    }
+
+    public byte getbMinutos() {
+        return bMinutos;
+    }
+
+    public byte getbSegundos() {
+        return bSegundos;
+    }
 }
