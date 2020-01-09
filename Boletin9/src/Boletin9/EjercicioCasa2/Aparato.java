b0fd1324962f11a408dfd2ae6fbf413a8231b4fc
pabcavper <gelves.mula@gmail.com>
2020-01-09 10:46:10 +0100

    Boletin 9 Aparato

---
+++ Boletin9/src/Boletin9/EjercicioCasa2/Aparato.java
@@ -0,0 +1,23 @@
+package Boletin9.EjercicioCasa2;
+
+public class Aparato {
+    private float fConsumo;
+    private boolean isEncendido;
+
+    public Aparato(){
+        setEncendido(false);
+    }
+
+    public void setEncendido(boolean encendido) {
+        isEncendido = encendido;
+    }
+
+    public boolean setfConsumo(float fConsumo) {
+        boolean bExito = false;
+        if (fConsumo > 0){
+            this.fConsumo = fConsumo;
+            bExito = true;
+        }
+        return bExito;
+    }
+}
