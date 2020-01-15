6772a324cd23df04767075191041efd14d6a715a
pabcavper <gelves.mula@gmail.com>
2020-01-15 14:07:39 +0100

    Boletin 10 ExperimentoAvion

--- Boletin10/src/ExperimentoAvion/Avion.java
+++ Boletin10/src/ExperimentoAvion/Avion.java
@@ -5,7 +5,28 @@ public abstract class Avion implements IAeronave{
     private String sId;
     private boolean trenAbierto;
 
+    public Avion(int iAltura, String sId) {
+        this.setiAltura(iAltura);
+        this.sId = sId;
+    }
 
+    public void setiAltura(int iAltura) {
+        this.iAltura = iAltura;
+    }
 
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
 
+    public void setTrenAbierto(boolean trenAbierto) {
+        this.trenAbierto = trenAbierto;
+    }
+
+    public int getiAltura() {
+        return iAltura;
+    }
+
+    public String getsId() {
+        return sId;
+    }
 }
