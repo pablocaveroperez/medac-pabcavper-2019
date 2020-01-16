cee016536f2ec82fab894e7e004d798b58b6bd4c
pabcavper <gelves.mula@gmail.com>
2020-01-16 09:38:06 +0100

    Boletin 10 EjercicioCasa3

--- Boletin10/src/EjercicioCasa3/IPublicacion.java
+++ Boletin10/src/EjercicioCasa3/IPublicacion.java
@@ -2,4 +2,24 @@ package EjercicioCasa3;
 
 public interface IPublicacion {
 
+    public int getCodigo();
+
+    public void setCodigo(int iCodigo);
+
+    public String getAutor();
+
+    public void setAutor(String sAutor);
+
+    public String getTitulo();
+
+    public void setTitulo(String sTitulo);
+
+    public short getshPublicacion();
+
+    public void setshPublicacion(short shPublicacion);
+
+    public boolean getEstaPrestado();
+
+    public void setEstaPrestado(boolean estaPrestado);
+
 }
