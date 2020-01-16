d5e3a6cdd6cb9393e3d5d82d21c31b5a722db1fa
pabcavper <gelves.mula@gmail.com>
2020-01-16 09:39:53 +0100

    Boletin 10 EjercicioCasa3

--- Boletin10/src/EjercicioCasa3/Publicacion.java
+++ Boletin10/src/EjercicioCasa3/Publicacion.java
@@ -1,6 +1,6 @@
 package EjercicioCasa3;
 
-public class Publicacion {
+public class Publicacion implements IPublicacion {
     protected int iCodigo;
     protected String sAutor;
     protected String sTitulo;
@@ -12,4 +12,53 @@ public class Publicacion {
     }
 
 
+    @Override
+    public int getCodigo() {
+        return this.iCodigo;
+    }
+
+    @Override
+    public void setCodigo(int iCodigo) {
+
+    }
+
+    @Override
+    public String getAutor() {
+        return this.sAutor;
+    }
+
+    @Override
+    public void setAutor(String sAutor) {
+
+    }
+
+    @Override
+    public String getTitulo() {
+        return this.sTitulo;
+    }
+
+    @Override
+    public void setTitulo(String sTitulo) {
+
+    }
+
+    @Override
+    public short getshPublicacion() {
+        return this.shPubicacion;
+    }
+
+    @Override
+    public void setshPublicacion(short shPublicacion) {
+
+    }
+
+    @Override
+    public boolean getEstaPrestado() {
+        return this.estaPrestado;
+    }
+
+    @Override
+    public void setEstaPrestado(boolean estaPrestado) {
+
+    }
 }
