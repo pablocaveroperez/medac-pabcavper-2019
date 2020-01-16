615bb2620e083c0028695bc90c82a21bc1a5eceb
pabcavper <gelves.mula@gmail.com>
2020-01-16 10:11:02 +0100

    Boletin 10 EjercicioCasa3

--- Boletin10/src/EjercicioCasa3/Disco.java
+++ Boletin10/src/EjercicioCasa3/Disco.java
@@ -1,69 +1,9 @@
 package EjercicioCasa3;
 
-public class Disco extends Publicacion implements IPublicacion {
+public class Disco extends Publicacion {
     private String sTitulo;
 
     public Disco(int iCodigo, String sAutor, String sTitulo, short shPubicacion){
         super(iCodigo, sAutor, sTitulo, shPubicacion);
-    }
-
-    @Override
-    public int getCodigo() {
-        return this.iCodigo;
-    }
-
-    @Override
-    public boolean setCodigo(int iCodigo) {
-        boolean bExito = false;
-        if (iCodigo > 0){
-            this.iCodigo = iCodigo;
-            bExito = false;
-        }
-        return bExito;
-    }
-
-    @Override
-    public String getAutor() {
-        return this.sAutor;
-    }
-
-    @Override
-    public void setAutor(String sAutor) {
-        this.sAutor = sAutor;
-    }
-
-    @Override
-    public String getTitulo() {
-        return this.sTitulo;
-    }
-
-    @Override
-    public void setTitulo(String sTitulo) {
-        this.sTitulo = sTitulo;
-    }
-
-    @Override
-    public short getshPubicacion() {
-        return this.shPubicacion;
-    }
-
-    @Override
-    public boolean setshPubicacion(short shPubicacion) {
-        boolean bExito = false;
-        if (shPubicacion > 0){
-            this.shPubicacion = shPubicacion;
-            bExito = true;
-        }
-        return bExito;
-    }
-
-    @Override
-    public boolean getEstaPrestado() {
-        return this.estaPrestado;
-    }
-
-    @Override
-    public void setEstaPrestado(boolean estaPrestado) {
-        this.estaPrestado = estaPrestado;
     }
 }
--- Boletin10/src/EjercicioCasa3/mainPubicacion.java
+++ Boletin10/src/EjercicioCasa3/mainPubicacion.java
@@ -3,5 +3,8 @@ package EjercicioCasa3;
 public class mainPubicacion {
     public static void main(String[] args) {
         Publicacion publicacion1 = new Libro(1,"Edu Aguirre", "Luna de Pluton", (short) 123);
+        Publicacion publicacion2 = new Revista(2,"Edu Aguirre", "Chiringuito de jugones", (short) 123);
+        Publicacion publicacion3 = new Disco(3,"Edu Aguirre", "CR7 campeon", (short) 123);
+
     }
 }
