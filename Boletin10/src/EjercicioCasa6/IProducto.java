3235f84a1090313847fb36f5c2796ea5a56b76af
pabcavper <gelves.mula@gmail.com>
2020-01-20 10:01:52 +0100

    boletin10 casa 6

--- Boletin10/src/EjercicioCasa6/IProducto.java
+++ Boletin10/src/EjercicioCasa6/IProducto.java
@@ -1,4 +1,22 @@
 package EjercicioCasa6;
 
 public interface IProducto {
+
+    public float valorEnStock();
+
+    public void incrementarStock(int incremento) ;
+
+    public void disminuirStock(int decremento) ;
+
+    public String getNombre() ;
+
+    public float getPrecio() ;
+
+    public int getUnidades() ;
+
+    public void setNombre(String nombre) ;
+
+    public void setUnidades(int unidades) ;
+
+    public void setPrecio(float precio) ;
 }
