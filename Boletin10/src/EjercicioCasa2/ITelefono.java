c2f4c3770f6382ce8adb668fe4a04d28c88faa4c
pabcavper <gelves.mula@gmail.com>
2020-01-20 10:16:41 +0100

    boletin10 casa 6

--- Boletin10/src/EjercicioCasa2/ITelefono.java
+++ Boletin10/src/EjercicioCasa2/ITelefono.java
@@ -1,5 +1,15 @@
 package EjercicioCasa2;
 
 public interface ITelefono {
-    public
+    public int consultarNumero();
+
+    public void marcar();
+
+    public void colgar();
+
+    public void activarAltavoz();
+
+    public void desactivarAltavoz();
+
+    public boolean sonarTimbre();
 }
