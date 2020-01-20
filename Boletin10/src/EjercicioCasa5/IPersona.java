386323ee67ed175a34bf396be9d2bdfb2d05e10c
pabcavper <gelves.mula@gmail.com>
2020-01-20 09:47:19 +0100

    boletin10 casa 4

--- Boletin10/src/EjercicioCasa5/IPersona.java
+++ Boletin10/src/EjercicioCasa5/IPersona.java
@@ -6,4 +6,32 @@ public interface IPersona {
     public final byte SEPARADO = 3;
     public final byte DIVORCIADO = 4;
     public final byte VIUDO = 5;
+
+    public String getsNombre() ;
+
+    public void setsNombre(String sNombre) ;
+
+    public String getsApellido1() ;
+
+    public void setsApellido1(String sApellido1) ;
+
+    public String getsApellido2() ;
+
+    public void setsApellido2(String sApellido2) ;
+
+    public short getShEdad() ;
+
+    public boolean setbEdad(short shEdad) ;
+
+    public float getfEstatura() ;
+
+    public boolean setfEstatura(float fEstatura) ;
+
+    public float getfKilos();
+
+    public boolean setfKilos(float fKilos);
+
+    public byte getbEstado();
+
+    public boolean setbEstado(byte bEstado);
 }
