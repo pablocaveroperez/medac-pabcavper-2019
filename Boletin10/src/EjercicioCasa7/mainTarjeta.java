36a92e7d225b9f7ed2ffde7a7d2cef35379fe711
pabcavper <gelves.mula@gmail.com>
2020-01-20 13:12:01 +0100

    boletin10 casa 7

--- Boletin10/src/EjercicioCasa7/mainTarjeta.java
+++ Boletin10/src/EjercicioCasa7/mainTarjeta.java
@@ -2,7 +2,8 @@ package EjercicioCasa7;
 
 public class mainTarjeta {
     public static void SimulaCompra(ITarjetaMonedero tarjetaMonedero){
-        System.out.println();
+        System.out.println("Introduce el DNI: ");
+
     }
 
     public static void simulaRetiradaCajero(ITarjetaCredito tarjetaCredito){
