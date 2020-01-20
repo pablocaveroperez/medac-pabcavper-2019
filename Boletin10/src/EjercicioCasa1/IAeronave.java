07f5b45eedbdc2c182a890e67020424140c03f5e
pabcavper <gelves.mula@gmail.com>
2020-01-20 12:26:28 +0100

    boletin10 casa 1

--- Boletin10/src/EjercicioCasa1/IAeronave.java
+++ Boletin10/src/EjercicioCasa1/IAeronave.java
@@ -1,4 +1,12 @@
 package EjercicioCasa1;
 
 public interface IAeronave {
+
+    public String aumentarAlturaDeVuelo(int aumento);
+
+    public String disminuirAlturaDeVuelo(int descenso);
+
+    public String despegar();
+
+    public String aterrizar();
 }
