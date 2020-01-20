6f1c50bd4317b6bd3e15eae843bd0c06307e4df2
pabcavper <gelves.mula@gmail.com>
2020-01-20 09:32:16 +0100

    boletin10 casa 4

---
+++ Boletin10/src/EjercicioCasa4/IExpendedora.java
@@ -0,0 +1,10 @@
+package EjercicioCasa4;
+
+public interface IExpendedora {
+
+    public void insertarMoneda(float valorMoneda);
+
+    public void pedirProducto(float costeProducto);
+
+    public void devolucionCreditoRestante();
+}
