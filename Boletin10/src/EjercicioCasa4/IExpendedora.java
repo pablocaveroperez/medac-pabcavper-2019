1b646858358978cce4e48235222744733427cff7
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-01-24 20:47:56 +0100

    Boletin 10 Expendedora

--- Boletin10/src/EjercicioCasa4/IExpendedora.java
+++ Boletin10/src/EjercicioCasa4/IExpendedora.java
@@ -2,6 +2,22 @@ package EjercicioCasa4;
 
 public interface IExpendedora {
 
+    int getiNumeroSerie();
+
+    void setiNumeroSerie(int iNumeroSerie);
+
+    float getiCreditoAcumulado();
+
+    void setiCreditoAcumulado(float iCreditoAcumulado);
+
+    float getiCreditoConsumido();
+
+    void setiCreditoConsumido(float iCreditoConsumido);
+
+    double getiDineroAcumulado();
+
+    void setiDineroAcumulado(double iDineroAcumulado);
+
     public void insertarMoneda(float valorMoneda);
 
     public void pedirProducto(float costeProducto);
