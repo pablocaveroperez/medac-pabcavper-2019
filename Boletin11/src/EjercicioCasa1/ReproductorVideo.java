fa3ccca5b59ef860556c3f8320591764604fdbd0
pabcavper <gelves.mula@gmail.com>
2020-01-29 13:33:15 +0100

    BOLETIN11 ReproductorVideo

--- Boletin11/src/EjercicioCasa1/ReproductorVideo.java
+++ Boletin11/src/EjercicioCasa1/ReproductorVideo.java
@@ -12,50 +12,62 @@ public class ReproductorVideo extends ReproductorPortatil implements IReproducto
         super(marca, modelo, tipoDeAlmacenamiento);
     }
 
+    @Override
     public boolean isReproduceWMV() {
         return reproduceWMV;
     }
 
+    @Override
     public void setReproduceWMV(boolean reproduceWMV) {
         this.reproduceWMV = reproduceWMV;
     }
 
+    @Override
     public boolean isReproduceDIVX() {
         return reproduceDIVX;
     }
 
+    @Override
     public void setReproduceDIVX(boolean reproduceDIVX) {
         this.reproduceDIVX = reproduceDIVX;
     }
 
+    @Override
     public boolean isReproduceMPG() {
         return reproduceMPG;
     }
 
+    @Override
     public void setReproduceMPG(boolean reproduceMPG) {
         this.reproduceMPG = reproduceMPG;
     }
 
+    @Override
     public boolean isReproduceDVD() {
         return reproduceDVD;
     }
 
+    @Override
     public void setReproduceDVD(boolean reproduceDVD) {
         this.reproduceDVD = reproduceDVD;
     }
 
+    @Override
     public boolean isReproduceJPG() {
         return reproduceJPG;
     }
 
+    @Override
     public void setReproduceJPG(boolean reproduceJPG) {
         this.reproduceJPG = reproduceJPG;
     }
 
+    @Override
     public float getTamanioDeLaPantalla() {
         return tamanioDeLaPantalla;
     }
 
+    @Override
     public boolean setTamanioDeLaPantalla(float tamanioDeLaPantalla) {
         boolean bExito = false;
         if (tamanioDeLaPantalla > 0){
@@ -65,11 +77,20 @@ public class ReproductorVideo extends ReproductorPortatil implements IReproducto
         return bExito;
     }
 
+    @Override
     public boolean isTelevision() {
         return television;
     }
 
+    @Override
     public void setTelevision(boolean television) {
         this.television = television;
+    }
+
+    @Override
+    public String toString (){
+        String salida = "";
+
+        return salida;
     }
 }
