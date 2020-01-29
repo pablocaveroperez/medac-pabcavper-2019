b76b0f88db8379baab646114558fbea40308a79f
pabcavper <gelves.mula@gmail.com>
2020-01-29 11:35:35 +0100

    BOLETIN11 ReproductorDeAudio

--- Boletin11/src/EjercicioClase2/ReproductorDeAudio.java
+++ Boletin11/src/EjercicioClase2/ReproductorDeAudio.java
@@ -1,6 +1,6 @@
 package EjercicioClase2;
 
-public class ReproductorDeAudio {
+public class ReproductorDeAudio implements IReproductorAudio {
     private boolean reproduceAudioCds, reproduceMP3, reproduceWMA, reproduceVorbis;
     private boolean accesoPorCarpetas;
     private boolean radio;
