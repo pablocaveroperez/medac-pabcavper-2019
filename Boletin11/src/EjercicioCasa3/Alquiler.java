92f163c5a0edde445f29a5f829d5b705895ff85a
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-02 13:46:40 +0100

    Boletin11 EjercicioCasa3 Alquiler

---
+++ Boletin11/src/EjercicioCasa3/Alquiler.java
@@ -0,0 +1,23 @@
+package EjercicioCasa3;
+
+public class Alquiler {
+    private int idAlquiler;
+    private String fecha;
+    private String hora;
+    private String tipo;
+
+    public Alquiler(Socio s1){
+
+    }
+
+    public void guardarLiner(LineaAlquiler lineaAlquiler){
+
+    }
+
+    public void eliminarLinea(LineaAlquiler lineaAlquiler){
+
+    }
+    public void imprimirRecibo(){
+
+    }
+}
