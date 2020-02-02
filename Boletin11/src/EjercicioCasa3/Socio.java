6a7a82e4d594a52d17eb575ef0466dce4e4dedd2
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-02 12:17:43 +0100

    Boletin11 EjercicioCasa3

--- Boletin11/src/EjercicioCasa3/Socio.java
+++ Boletin11/src/EjercicioCasa3/Socio.java
@@ -9,5 +9,59 @@ public class Socio {
     private String email;
     private String direccion;
 
+    public int getNumeroSocio() {
+        return numeroSocio;
+    }
 
+    private void setNumeroSocio(int numeroSocio) {
+        this.numeroSocio = numeroSocio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 }
