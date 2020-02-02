54f7199992c75cd3e36ace7ff5c62d92f292c245
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-02 13:58:31 +0100

    Boletin11 EjercicioCasa3 Alquiler LineaAlquiler Descripcion

--- Boletin11/src/EjercicioCasa3/Descripcion.java
+++ Boletin11/src/EjercicioCasa3/Descripcion.java
@@ -1,4 +1,28 @@
 package EjercicioCasa3;
 
 public class Descripcion {
+    private String descripcion;
+
+    public Descripcion(){
+
+    }
+
+    public Descripcion(String descripcion){
+
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        String salida = "";
+        salida += "Descripcion: "+getDescripcion()+"\n";
+        return salida;
+    }
 }
