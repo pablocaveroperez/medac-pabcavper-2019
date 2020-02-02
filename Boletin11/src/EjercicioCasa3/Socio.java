722b65ca65a8fdfa6403c711a9e69ea7627e332d
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-02 12:17:05 +0100

    Boletin11 EjercicioCasa3

--- Boletin11/src/EjercicioCasa3/Socio.java
+++ Boletin11/src/EjercicioCasa3/Socio.java
@@ -1,4 +1,13 @@
 package EjercicioCasa3;
 
 public class Socio {
+    private int numeroSocio;
+    private int telefono;
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String direccion;
+
+
 }
