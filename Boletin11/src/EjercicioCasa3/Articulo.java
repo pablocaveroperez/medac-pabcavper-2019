f17e3707eb0350244027d25ffb407b2c4ace9178
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-02 13:53:03 +0100

    Boletin11 EjercicioCasa3 Alquiler LineaAlquiler

---
+++ Boletin11/src/EjercicioCasa3/Articulo.java
@@ -0,0 +1,44 @@
+package EjercicioCasa3;
+
+public class Articulo {
+    private int idArticulo;
+    private int tamanio;
+    private int stock;
+    private String nombre;
+
+    public Articulo(){
+
+    }
+
+    public int getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(int idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setTamanio(int tamanio) {
+        this.tamanio = tamanio;
+    }
+}
