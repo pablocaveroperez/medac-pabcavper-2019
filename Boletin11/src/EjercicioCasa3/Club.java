0ad6d48031151b3d33caa9f323d30e30a2e2db89
pabcavper <gelves.mula@gmail.com>
2020-02-03 14:47:11 +0100

    BOLETIN11 RegistroReserva Terminal Club

--- Boletin11/src/EjercicioCasa3/Club.java
+++ Boletin11/src/EjercicioCasa3/Club.java
@@ -9,5 +9,10 @@ public class Club {
 
     public static void main(String[] args) {
         Terminal t1 = new Terminal((byte) 1,MAXSOCIOS,MAXARTICULOS,MAXINSTALACIONES,MAXRESERVA,MAXALQUILERES);
+
+        Socio s1 = new Socio();
+
+        t1.addSocio(s1);
+        System.out.println(t1.printSocio());
     }
 }
