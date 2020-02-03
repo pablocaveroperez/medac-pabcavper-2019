7ac13016cbef0eef27fda6b2f09cfe35ad7d88e8
pabcavper <gelves.mula@gmail.com>
2020-02-03 09:14:21 +0100

    BOLETIN11 Catalogo

--- Boletin11/src/EjercicioCasa3/Catalogo.java
+++ Boletin11/src/EjercicioCasa3/Catalogo.java
@@ -2,16 +2,25 @@ package EjercicioCasa3;
 
 public class Catalogo {
     private int autoincremento;
+    private Articulo[] articulos;
 
     public Catalogo(){
 
     }
 
-    public Descripcion getDescripcion(){
-
+    public Articulo[] getArticulos() {
+        return articulos;
     }
 
-    public void setDescripcion(Descripcion descripcion){
+    public void setArticulos(Articulo[] articulos) {
+        this.articulos = articulos;
+    }
 
+    public int getAutoincremento() {
+        return autoincremento;
+    }
+
+    public void setAutoincremento(int autoincremento) {
+        this.autoincremento = autoincremento;
     }
 }
