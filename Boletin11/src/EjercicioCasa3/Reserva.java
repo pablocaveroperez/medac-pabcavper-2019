759d6dedbfca9c15f533b02b991fb659037539b2
pabcavper <gelves.mula@gmail.com>
2020-02-03 09:48:49 +0100

    BOLETIN11 Reserva

---
+++ Boletin11/src/EjercicioCasa3/Reserva.java
@@ -0,0 +1,20 @@
+package EjercicioCasa3;
+
+public class Reserva {
+    private int idReserva;
+    private String fecha;
+    private String hora;
+
+    public Reserva(){
+
+    }
+
+    public void introducirDatos(Socio s1, Instalacion i1){
+
+    }
+
+    public void imprimirRecibo(){
+
+    }
+
+}
