363d673a57e28b8856f1cf6ba1752a1424f02e8c
pabcavper <gelves.mula@gmail.com>
2020-02-12 08:54:08 +0100

    Boletin12

---
+++ Boletin12/src/EjercicioClase1/Reloj.java
@@ -0,0 +1,108 @@
+package EjercicioClase1;
+
+
+public class Reloj {
+    private byte bHoras;
+    private byte bMinutos;
+    private byte bSegundos;
+
+    public Reloj(){
+        setbHoras((byte)0);
+        setbMinutos((byte)0);
+        setbSegundos((byte)0);
+    }
+
+    public Reloj(byte bHoras, byte bMinutos, byte bSegundos){
+        setbHoras(bHoras);
+        setbMinutos(bMinutos);
+        setbSegundos(bSegundos);
+    }
+
+    public boolean setbHoras(byte bHoras) {
+        boolean bExito = false;
+        if (bHoras >= 0 && bHoras <= 24){
+            this.bHoras = bHoras;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public boolean setbMinutos(byte bMinutos) {
+        boolean bExito = false;
+        if (bMinutos >= 0 && bMinutos <= 59){
+            this.bMinutos = bMinutos;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public boolean setbSegundos(byte bSegundos) {
+        boolean bExito = false;
+        if (bSegundos >= 0 && bSegundos <= 59){
+            this.bSegundos = bSegundos;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public byte getbHoras() {
+        return bHoras;
+    }
+
+    public byte getbMinutos() {
+        return bMinutos;
+    }
+
+    public byte getbSegundos() {
+        return bSegundos;
+    }
+
+    public String darHora(byte bTipo, Reloj reloj){
+        String salida ="";
+        if (bTipo == 1){ // 12
+            salida += darHora12(reloj);
+        }else if (bTipo == 2){ // 24
+            salida += darHora24();
+        }
+        return salida;
+    }
+
+    private String darHora24() {
+        return imprimir();
+    }
+
+    private String darHora12(Reloj reloj) {
+        String salida = "";
+        if (reloj.getbHoras() <= 11 && reloj.getbHoras() >= 0){
+            salida += reloj.getbHoras()+ ":"+reloj.getbMinutos()+":"+reloj.getbSegundos()+" AM";
+        }else if (reloj.getbHoras() > 12 && reloj.getbHoras() <= 23){
+            salida += (reloj.getbHoras() - 12)+":"+reloj.getbMinutos()+":"+reloj.getbSegundos()+" PM";
+        }else if (reloj.getbHoras() == 12){
+            salida += toString()+ " PM";
+        }else if (reloj.getbHoras() == 24)
+            salida += "00:"+reloj.getbMinutos()+":"+reloj.getbSegundos()+" AM";
+        return salida;
+    }
+
+    public void ponerEnHora(byte bHoras, byte bMinutos){
+        setbHoras(bHoras);
+        setbMinutos(bMinutos);
+        setbSegundos((byte)0);
+    }
+
+    public void ponerEnHora(byte bHoras, byte bMinutos, byte bSegundos){
+        setbHoras(bHoras);
+        setbMinutos(bMinutos);
+        setbSegundos(bSegundos);
+    }
+
+    private String imprimir() {
+        String salida = "";
+        if (getbHoras() == 24)
+            salida += "00:";
+        else
+            salida += getbHoras() + ":";
+        salida += getbMinutos()+":"+getbSegundos();
+        return salida;
+    }
+}
---
+++ Boletin12/src/EjercicioClase1/mainReloj.java
@@ -0,0 +1,25 @@
+package EjercicioClase1;
+
+import static EjercicioClase1.Libreria.valida;
+
+public class mainReloj {
+    public static void main(String[] args) {
+        byte bHoras = (byte) valida("Introduce las horas(0-24): ",0,24,3);
+        byte bMinutos = (byte) valida("Introduce los minutos(0-59): ",0,59,3);
+        byte bSegundos = (byte) valida("Introduce las segundos(0-59): ",0,59,3);
+
+
+        Reloj reloj1 = new Reloj(bHoras,bMinutos,bSegundos);
+
+        System.out.println(reloj1.darHora((byte)1,reloj1));
+        System.out.println(reloj1.darHora((byte)2,reloj1));
+
+        bHoras = (byte) valida("Introduce las horas(0-24): ",0,24,3);
+        bMinutos = (byte) valida("Introduce los minutos(0-59): ",0,59,3);
+
+
+        reloj1.ponerEnHora(bHoras,bMinutos);
+        System.out.println(reloj1.darHora((byte)1,reloj1));
+        System.out.println(reloj1.darHora((byte)2,reloj1));
+    }
+}
