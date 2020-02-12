24c35c808d1c8656f69904e7f4673fa874f8fe2a
pabcavper <gelves.mula@gmail.com>
2020-02-12 14:04:03 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Modelos/Cliente.java
+++ Boletin12/src/Tienda/Modelos/Cliente.java
@@ -6,6 +6,23 @@ public class Cliente {
     private String sApellidos;
     private String sDni;
 
+    public Cliente(){
 
+    }
 
+    public void setsNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setsApellidos(String sApellidos) {
+        this.sApellidos = sApellidos;
+    }
+
+    public void setsDni(String sDni) {
+        this.sDni = sDni;
+    }
 }
