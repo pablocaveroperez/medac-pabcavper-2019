7459f9772e0b5f764cef6fce9c62e1ba92edb8f5
pabcavper <gelves.mula@gmail.com>
2020-02-12 09:00:24 +0100

    Boletin12

--- Boletin12/src/EjercicioClase1/mainReloj.java
+++ Boletin12/src/EjercicioClase1/mainReloj.java
@@ -4,16 +4,25 @@ import static EjercicioClase1.Libreria.valida;
 
 public class mainReloj {
     public static void main(String[] args) {
+        byte bHoras = 0, bMinutos, bSegundos;
 
-        try {
-            byte bHoras = (byte) valida("Introduce las horas(0-24): ",0,24,3);
-        }catch (NumberFormatException exc){
+        do {
+            try {
+                bHoras = (byte) valida("Introduce las horas(0-24): ",0,24,3);
+            }catch (NumberFormatException exc){
+                bHoras = -1;
+            }finally {
+                if (bHoras == -1){
+                    System.out.println("La hora introducida es incorrecta.");
+                }
+                else System.out.println("La hora introducida es correcta");
+            }
+        }while (bHoras == -1);
 
-        }
 
 
-        byte bMinutos = (byte) valida("Introduce los minutos(0-59): ",0,59,3);
-        byte bSegundos = (byte) valida("Introduce las segundos(0-59): ",0,59,3);
+        bMinutos = (byte) valida("Introduce los minutos(0-59): ",0,59,3);
+        bSegundos = (byte) valida("Introduce las segundos(0-59): ",0,59,3);
 
 
         Reloj reloj1 = new Reloj(bHoras,bMinutos,bSegundos);
