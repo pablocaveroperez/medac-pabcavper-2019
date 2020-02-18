1917bb4fbbfcee899feee4a6bc6138e8a7b834fb
pabcavper <gelves.mula@gmail.com>
2020-02-18 12:49:58 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Vistas/ClienteView.java
+++ Boletin12/src/Tienda/Vistas/ClienteView.java
@@ -19,6 +19,10 @@ public class ClienteView {
         saldo = (float) LibFrontend.valida("Introduce el saldo del socio: ",0,2000,2);
         idCliente = (int) LibFrontend.valida("Introduce el ID del socio",0,-1,1);
         Cliente oCliente = new Cliente(sDni,sNombre,sApellidos,saldo,idCliente);
-
+        if (!(tienda.getClienteController().add(oCliente)))
+            sResultado = "El socio no se ha podido registrar correctamente";
+        else
+            sResultado = "El socio se ha podido registrar correctamente";
+        return sResultado;
     }
 }
