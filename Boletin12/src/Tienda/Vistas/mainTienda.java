b06e920ef496d98ba2fce48648be96ea14021da2
pabcavper <gelves.mula@gmail.com>
2020-02-18 13:20:00 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Vistas/mainTienda.java
+++ Boletin12/src/Tienda/Vistas/mainTienda.java
@@ -1,20 +1,22 @@
 package Tienda.Vistas;
 
 import Tienda.Controladores.ClienteController;
+import Tienda.Controladores.Tienda;
 import Tienda.Modelos.Cliente;
 
 public class mainTienda {
     public static void main(String[] args) {
-        int iPosicion = 3;
-        ClienteController clienteController = new ClienteController();
+        Tienda tienda = new Tienda();
 
-        clienteController.add(new Cliente(1));
-        clienteController.add(new Cliente(2));
-        clienteController.add(new Cliente(3));
-        clienteController.add(new Cliente(4));
-        clienteController.add(new Cliente(5));
+        // ADD CLIENTE
+        System.out.println(ClienteView.altaCliente(tienda));
+        System.out.println("Numero de socios registrados en la tienda: "+ tienda.getClienteController().getbContadorArray());
 
-        clienteController.remove(new Cliente(3));
+        // ADD ARTICULO
+        System.out.println(ArticuloView.altaArticulo(tienda));
+        System.out.println("Numero de articulos registrados en la tienda: "+ tienda.getArticuloController().getbContadorArray());
 
+        System.out.println("Estos son todos los clientes registrados: \n" + tienda.getClienteController().printAll());
+        System.out.println("Estos son todos los articulos registrados: \n" + tienda.getArticuloController().printAll());
     }
 }
