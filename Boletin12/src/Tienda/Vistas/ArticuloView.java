45285f973918c9681da2a83c3c9a79dbc8be5084
pabcavper <gelves.mula@gmail.com>
2020-02-18 13:11:37 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Vistas/ArticuloView.java
+++ Boletin12/src/Tienda/Vistas/ArticuloView.java
@@ -1,9 +1,25 @@
 package Tienda.Vistas;
 
+import Tienda.Controladores.Tienda;
+import Tienda.Modelos.Articulo;
+
+import static medac.validaciones.LibFrontend.*;
+
 public class ArticuloView {
     public static String altaArticulo(Tienda tienda){
         int id;
         String sNombre;
         float precio;
+        String sResultado;
+
+        id = (int) valida("Instroduce el ID del articulo: ",0,-1,1);
+        sNombre = leer("Introduce el nombre del articulo: ");
+        precio = (float) valida("Introduce el precio del articulo: ",0,500,2);
+
+        Articulo articulo = new Articulo(id,sNombre,precio);
+        if (!(tienda.getArticuloController().add(articulo)))
+            sResultado = "El articulo no se ha podido registrar correctamente";
+        else
+            sResultado = "El articulo se ha podido registrar correctamente";
     }
 }
