e3d90965569e7dae6441b93363148aee2198b1b9
pabcavper <gelves.mula@gmail.com>
2020-02-18 12:47:41 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Controladores/Tienda.java
+++ Boletin12/src/Tienda/Controladores/Tienda.java
@@ -2,4 +2,19 @@ package Tienda.Controladores;
 
 
 public class Tienda {
+    private ClienteController clienteController;
+    private ArticuloController articuloController;
+
+    public Tienda(){
+        clienteController = new ClienteController();
+        articuloController = new ArticuloController();
+    }
+
+    public ArticuloController getArticuloController() {
+        return articuloController;
+    }
+
+    public ClienteController getClienteController() {
+        return clienteController;
+    }
 }
