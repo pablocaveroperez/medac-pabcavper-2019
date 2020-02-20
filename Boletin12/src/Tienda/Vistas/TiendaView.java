c5808d80948aad16ef2f74eebb5dad03268d413b
pabcavper <gelves.mula@gmail.com>
2020-02-20 11:40:12 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Vistas/TiendaView.java
+++ Boletin12/src/Tienda/Vistas/TiendaView.java
@@ -1,4 +1,23 @@
 package Tienda.Vistas;
 
+import Tienda.Controladores.Tienda;
+
 public class TiendaView {
+    private Tienda tienda;
+    private ArticuloView articuloView;
+    private ClienteView clienteView;
+
+    public ArticuloView getArticuloView() {
+        return articuloView;
+    }
+
+    public ClienteView getClienteView() {
+        return clienteView;
+    }
+
+    public Tienda getTienda() {
+        return tienda;
+    }
+
+
 }
