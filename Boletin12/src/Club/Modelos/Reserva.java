ba24e158a100a0270b80c32b2471395d9218b71c
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-23 10:48:56 +0100

    Boletin 12 Club Deportivo

--- Boletin12/src/Club/Modelos/Reserva.java
+++ Boletin12/src/Club/Modelos/Reserva.java
@@ -1,5 +1,66 @@
 package Club.Modelos;
 
+import java.util.Calendar;
+
 public class Reserva {
     private Socio socio;
+    private Instalacion instalacion;
+    private Calendar fecha;
+    private byte hora;
+    private byte minutos;
+
+    public Reserva(Socio socio, Instalacion instalacion, Calendar fecha, byte hora, byte minutos){
+        setSocio(socio);
+        setInstalacion(instalacion);
+        setFecha(fecha);
+        setHora(hora);
+        setMinutos(minutos);
+    }
+
+    public Socio getSocio() {
+        return socio;
+    }
+
+    private void setSocio(Socio socio) {
+        this.socio = socio;
+    }
+
+    public Instalacion getInstalacion() {
+        return instalacion;
+    }
+
+    private void setInstalacion(Instalacion instalacion) {
+        this.instalacion = instalacion;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    private void setFecha(Calendar fecha) {
+        this.fecha = fecha;
+    }
+
+    public byte getHora() {
+        return hora;
+    }
+
+    private boolean setHora(byte hora) {
+        boolean bExito = false;
+        if (hora >= 0 && hora < 24){
+            this.hora = hora;
+            bExito = true;
+        }else
+            this.hora = -1;
+        return bExito;
+    }
+
+    public byte getMinutos() {
+        return minutos;
+    }
+
+    private boolean setMinutos(byte minutos) {
+
+        this.minutos = minutos;
+    }
 }
