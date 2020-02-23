5129095e088b2a8e481e51f74cb757da6877b88f
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-23 13:41:26 +0100

    Boletin 12 Club Deportivo

--- Boletin12/src/Club/Vistas/ReservaView.java
+++ Boletin12/src/Club/Vistas/ReservaView.java
@@ -8,6 +8,8 @@ import Tienda.Vistas.TiendaView;
 
 import java.util.Calendar;
 
+import static Libreria.LibreriaValida.*;
+
 public class ReservaView {
     public static void menuReservas(Terminal terminal) {
         byte opcion;
@@ -54,6 +56,19 @@ public class ReservaView {
         return bExito;
     }
 
+    private static String mostrarReserva(Terminal terminal) {
+        return null;
+    }
+
+    private static Reserva buscarReserva(Terminal terminal) {
+    }
+
+    private static boolean eliminarReserva(Terminal terminal) {
+    }
+
+    private static boolean modificarReserva(Terminal terminal) {
+    }
+
     private static boolean altaReserva(Terminal terminal) {
         boolean bExito = false;
         Socio socio;
@@ -61,6 +76,53 @@ public class ReservaView {
         Calendar fecha;
         byte hora;
         byte minutos;
+
+        do {
+            try {
+                socio = terminal.getSocioController().getaVector()[terminal.getSocioController().search(new Socio((int) valida("Introduce la id del socio: ",0,-1,1)))];
+                bExito = true;
+            }catch (NumberFormatException exc){
+                System.out.println("Error: " + exc.getMessage());
+            }catch (Exception exc){
+                System.out.println("Error generico: " + exc.getMessage());
+            }finally {
+                if (!bExito)
+                    System.out.println("ID del socio introducido incorrecto");
+            }
+        }while(!bExito);
+
+        byte ubicacion = 0, tipo = 0;
+        do {
+            bExito = false;
+            try {
+                ubicacion = (byte) valida("Introduce la ubicacion de la instalacion: ",0,-1,3);
+                bExito = true;
+            }catch (NumberFormatException exc){
+                System.out.println("Error: " + exc.getMessage());
+            }catch (Exception exc){
+                System.out.println("Error generico: " + exc.getMessage());
+            }finally {
+                if (!bExito)
+                    System.out.println("Ubicacion de la instalacion introducido incorrecto");
+            }
+        }while(!bExito);
+
+        do {
+            bExito = false;
+            try {
+                ubicacion = (byte) valida("Introduce el tipo de la instalacion: ",1,3,3);
+                bExito = true;
+            }catch (NumberFormatException exc){
+                System.out.println("Error: " + exc.getMessage());
+            }catch (Exception exc){
+                System.out.println("Error generico: " + exc.getMessage());
+            }finally {
+                if (!bExito)
+                    System.out.println("Tipo de la instalacion introducido incorrecto");
+            }
+        }while(!bExito);
+
+        instalacion= terminal.getInstalacionController().getaVector()[terminal.getInstalacionController().search(new Instalacion(ubicacion,tipo))];
 
     }
 }
