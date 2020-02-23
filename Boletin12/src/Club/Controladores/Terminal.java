2b56864dca577639726481178fc0840bf3f1b110
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-23 11:50:50 +0100

    Boletin 12 Club Deportivo

---
+++ Boletin12/src/Club/Controladores/Terminal.java
@@ -0,0 +1,36 @@
+package Club.Controladores;
+
+import Club.Modelos.Reserva;
+import Tienda.Controladores.ClienteController;
+
+public class Terminal {
+    private SocioController socioController;
+    private ReservaController reservaController;
+    private InstalacionController instalacionController;
+
+    public Terminal() {
+        socioController = new SocioController();
+        instalacionController = new InstalacionController();
+        reservaController = new ReservaController();
+    }
+
+    public SocioController getSocioController() {
+        return socioController;
+    }
+
+    public InstalacionController getInstalacionController() {
+        return instalacionController;
+    }
+
+    public ReservaController getReservaController() {
+        return reservaController;
+    }
+
+    public boolean add(Reserva reserva) {
+        boolean bExito = false;
+        if (socioController.search(reserva.getSocio()) != -1 && instalacionController.search(reserva.getInstalacion()) != -1)
+            bExito = reservaController.add(reserva);
+        return bExito;
+    }
+}
+
