6c0f7f97faab1d7e593652d060962807fdf0dc22
pabcavper <gelves.mula@gmail.com>
2020-02-24 11:20:53 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Modelos/LineaArticulo.java
+++ Boletin12/src/Tienda/Modelos/LineaArticulo.java
@@ -2,5 +2,26 @@ package Tienda.Modelos;
 
 public class LineaArticulo {
     private Articulo articulo;
-    private int cantidad;
+    private short cantidad;
+
+    public LineaArticulo(Articulo articulo, short cantidad){
+        setArticulo(articulo);
+        setCantidad(cantidad);
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public void setCantidad(short cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public short getCantidad() {
+        return cantidad;
+    }
 }
