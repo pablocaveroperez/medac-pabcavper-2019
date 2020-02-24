3d6ba598acdf2bc4ba7d689edc574ee7befd8d46
pabcavper <gelves.mula@gmail.com>
2020-02-24 14:11:20 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Modelos/Carrito.java
+++ Boletin12/src/Tienda/Modelos/Carrito.java
@@ -1,13 +1,34 @@
 package Tienda.Modelos;
 
 public class Carrito {
+    private byte idCarrito;
     private LineaArticulo[] lineaArticulos;
+    private final byte MAXLINEAARTICULOS = 100;
+    private byte contadorCarrito;
 
-    public Carrito(){
-
+    public Carrito() {
+        lineaArticulos = new LineaArticulo[MAXLINEAARTICULOS];
+        contadorCarrito = 0;
     }
 
-    public Carrito(LineaArticulo[] lineaArticulos){
+    public LineaArticulo[] getLineaArticulos() {
+        return lineaArticulos;
+    }
 
+    public byte getContadorCarrito() {
+        return contadorCarrito;
+    }
+
+    public void setContadorCarrito(byte contadorCarrito) {
+        this.contadorCarrito = contadorCarrito;
+    }
+
+    private void setIdCarrito(byte idCarrito) {
+
+        this.idCarrito = idCarrito;
+    }
+
+    public byte getIdCarrito() {
+        return idCarrito;
     }
 }
