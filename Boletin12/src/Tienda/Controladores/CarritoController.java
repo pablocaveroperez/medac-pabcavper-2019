1dd23c5d807a1a89052bb6c501d26de37b60896d
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-26 17:52:46 +0100

    Boletin 12 Tienda

--- Boletin12/src/Tienda/Controladores/CarritoController.java
+++ Boletin12/src/Tienda/Controladores/CarritoController.java
@@ -3,18 +3,18 @@ package Tienda.Controladores;
 import Tienda.Modelos.Carrito;
 
 public class CarritoController implements ICrud<Carrito> {
-    private Carrito[] carritos;
+    private Carrito[] vCarritos;
     private byte contadorCarritos;
     private final byte MAXCARRITOS = 50;
 
     public CarritoController(){
-        carritos = new Carrito[MAXCARRITOS];
+        vCarritos = new Carrito[MAXCARRITOS];
         contadorCarritos = 0;
     }
 
     @Override
     public Carrito[] getaVector() {
-        return carritos;
+        return vCarritos;
     }
 
     @Override
@@ -25,12 +25,26 @@ public class CarritoController implements ICrud<Carrito> {
     @Override
     public boolean add(Carrito oObject) {
         boolean bExito = false;
-
+        if (oObject.getIdCarrito() != 0 && contadorCarritos < MAXCARRITOS && search(oObject) == -1){
+            vCarritos[contadorCarritos] = oObject;
+            contadorCarritos++;
+            bExito = true;
+        }
+        return bExito;
     }
 
     @Override
     public boolean remove(Carrito oObject) {
-        return false;
+        boolean bExito = false;
+        int iPosicion = search(oObject);
+        if (iPosicion != -1){
+            for (int i = iPosicion; i < contadorCarritos; i++)
+                vCarritos[i] = vCarritos[i+1];
+            vCarritos[contadorCarritos] = null;
+            contadorCarritos--;
+            bExito = true;
+        }
+        return bExito;
     }
 
     @Override
