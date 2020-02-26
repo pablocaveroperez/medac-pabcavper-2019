a0fd4c2858a8f7993ccd6437af9e20922c86d402
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-26 18:16:20 +0100

    Revert "Boletin12 Tienda"
    This reverts commit 6c0f7f97

--- Boletin12/src/Tienda/Modelos/LineaArticulo.java
+++ Boletin12/src/Tienda/Modelos/LineaArticulo.java
@@ -1,58 +1,6 @@
 package Tienda.Modelos;
 
-public class LineaArticulo implements ILineaArticulo {
-    private int idLineaArticulo;
+public class LineaArticulo {
     private Articulo articulo;
-    private short cantidad;
-
-    public LineaArticulo(int idLineaArticulo, Articulo articulo, short cantidad){
-        setArticulo(articulo);
-        setCantidad(cantidad);
-    }
-
-    public LineaArticulo(int idLineaArticulo){
-        setArticulo(null);
-        setCantidad((short) 0);
-    }
-
-    @Override
-    public int getIdLineaArticulo() {
-        return idLineaArticulo;
-    }
-
-    private void setIdLineaArticulo(int idLineaArticulo) {
-        this.idLineaArticulo = idLineaArticulo;
-    }
-
-    @Override
-    public void setArticulo(Articulo articulo) {
-        this.articulo = articulo;
-    }
-
-    @Override
-    public boolean setCantidad(short cantidad) {
-        boolean bExito = false;
-        if (cantidad > 0 && cantidad < getArticulo().getStock()){
-            this.cantidad = cantidad;
-            bExito = true;
-        }
-        return bExito;
-    }
-
-    @Override
-    public Articulo getArticulo() {
-        return articulo;
-    }
-
-    @Override
-    public short getCantidad() {
-        return cantidad;
-    }
-
-    public String toString() {
-        String salida = "";
-        salida += articulo.toString();
-        salida += "\nCantidad seleccionada: " + getCantidad();
-        return salida;
-    }
+    private int cantidad;
 }
