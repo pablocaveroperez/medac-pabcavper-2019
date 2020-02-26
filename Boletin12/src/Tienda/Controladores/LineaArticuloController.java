daea028ff062dfb107ef3005310e5eb490a2a320
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-02-26 18:01:28 +0100

    Boletin 12 Tienda

--- Boletin12/src/Tienda/Controladores/LineaArticuloController.java
+++ Boletin12/src/Tienda/Controladores/LineaArticuloController.java
@@ -1,4 +1,49 @@
 package Tienda.Controladores;
 
-public class LineaArticuloController {
+import Tienda.Modelos.LineaArticulo;
+
+public class LineaArticuloController implements ICrud<LineaArticulo> {
+    private LineaArticulo[] vLineaArticulos;
+    private byte contadorLineaArticulos;
+    private final byte MAXLINEAARTICULOS = 100;
+
+    public LineaArticuloController(){
+        vLineaArticulos = new LineaArticulo[MAXLINEAARTICULOS];
+        contadorLineaArticulos = 0;
+    }
+
+    @Override
+    public LineaArticulo[] getaVector() {
+        return vLineaArticulos;
+    }
+
+    @Override
+    public byte getbContadorArray() {
+        return contadorLineaArticulos;
+    }
+
+    @Override
+    public boolean add(LineaArticulo oObject) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(LineaArticulo oObject) {
+        return false;
+    }
+
+    @Override
+    public int search(LineaArticulo oObject) {
+        return 0;
+    }
+
+    @Override
+    public String printAll() {
+        return null;
+    }
+
+    @Override
+    public boolean update(LineaArticulo oObjeto, int iPosicion) {
+        return false;
+    }
 }
