190d5f8b6571c87304397c81865f59af99e490b9
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-03-01 16:44:28 +0100

    Boletin12 Tienda

--- Boletin12/src/Tienda/Modelos/LineaArticulo.java
+++ Boletin12/src/Tienda/Modelos/LineaArticulo.java
@@ -3,4 +3,29 @@ package Tienda.Modelos;
 public class LineaArticulo {
     private Articulo articulo;
     private int cantidad;
+
+    public LineaArticulo() {
+
+    }
+
+    public LineaArticulo(Articulo articulo, int cantidad) {
+        setArticulo(articulo);
+        setCantidad(cantidad);
+    }
+
+    public void setArticulo(Articulo articulo) {
+        this.articulo = articulo;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Articulo getArticulo() {
+        return articulo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
 }
