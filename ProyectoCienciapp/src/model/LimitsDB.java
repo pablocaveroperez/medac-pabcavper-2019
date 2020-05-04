cf25ab843bade13bc48d0820460ae79939ceb68b
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-04 13:13:07 +0200

    Creada la clase ResearchTeam
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/model/LimitsDB.java
+++ ProyectoCienciapp/src/model/LimitsDB.java
@@ -2,6 +2,7 @@ package model;
 
 public interface LimitsDB {
     public final byte MAXCHAR_30 = 30;
+    public final byte MAXCHAR_40 = 40;
     public final byte MAXCHAR_50 = 50;
     public final byte MAXCHAR_60 = 60;
     public final byte MAXCHAR_65 = 65;
@@ -9,7 +10,7 @@ public interface LimitsDB {
     public final byte MAXCHAR_100 = 100;
     public final short MAXCHAR_200 = 200;
     public final short MAXCHAR_250 = 250;
-
+    public final int MAXBUDGET = 5000000;
 
     public final byte MINCHAR = 0;
 
---
+++ ProyectoCienciapp/src/model/ResearchTeam.java
@@ -0,0 +1,73 @@
+package model;
+
+public class ResearchTeam implements LimitsDB {
+    private byte idResearchTeam;
+    private Department oDepartment;
+    private String sName;
+    private int iBudget;
+
+    public ResearchTeam(byte idResearchTeam, Department oDepartment, String sName, int iBudget) {
+        setiBudget(iBudget);
+        setIdResearchTeam(idResearchTeam);
+        setoDepartment(oDepartment);
+        setsName(sName);
+    }
+
+    public ResearchTeam(byte idResearchTeam, Department oDepartment, int iBudget) {
+        setiBudget(iBudget);
+        setIdResearchTeam(idResearchTeam);
+        setoDepartment(oDepartment);
+    }
+
+    public byte getIdResearchTeam() {
+        return idResearchTeam;
+    }
+
+    private boolean setIdResearchTeam(byte idResearchTeam) {
+        boolean bExito = false;
+        if (idResearchTeam < MAXCHAR_30 && idResearchTeam > MINCHAR) {
+            this.idResearchTeam = idResearchTeam;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public Department getoDepartment() {
+        return oDepartment;
+    }
+
+    public boolean setoDepartment(Department oDepartment) {
+        boolean bExito = false;
+        if (oDepartment.checkDepartment()) {
+            this.oDepartment = oDepartment;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public boolean setsName(String sName) {
+        boolean bExito = false;
+        if (sName.length() > MINCHAR && sName.length() < MAXCHAR_40) {
+            this.sName = sName;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public int getiBudget() {
+        return iBudget;
+    }
+
+    public boolean setiBudget(int iBudget) {
+        boolean bExito = false;
+        if (iBudget > MINCHAR && iBudget < MAXBUDGET) {
+            this.iBudget = iBudget;
+            bExito = true;
+        }
+        return bExito;
+    }
+}
