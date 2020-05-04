eace1ebbcabd818329d32d9af831fe16accb626c
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-04 12:18:12 +0200

    Creada la clase Department
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

---
+++ ProyectoCienciapp/src/model/Department.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Department implements LimitsDB {
+    private byte idDepartment;
+    private Faculty oFaculty;
+    private String sName;
+
+    public Department(byte idDepartment, Faculty oFaculty, String sName) {
+        setIdDepartment(idDepartment);
+        setoFaculty(oFaculty);
+        setsName(sName);
+    }
+
+    public Department(byte idDepartment, Faculty oFaculty) {
+        setIdDepartment(idDepartment);
+        setoFaculty(oFaculty);
+    }
+
+    public byte getIdDepartment() {
+        return idDepartment;
+    }
+
+    private boolean setIdDepartment(byte idDepartment) {
+        boolean bExito = false;
+        if (idDepartment > MINCHAR && idDepartment < MAXCHAR_30) {
+            this.idDepartment = idDepartment;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public Faculty getoFaculty() {
+        return oFaculty;
+    }
+
+    public boolean setoFaculty(Faculty oFaculty) {
+        boolean bExito = false;
+        if (oFaculty.checkFaculty()) {
+            this.oFaculty = oFaculty;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public boolean setsName(String sName) {
+        boolean bExito = false;
+        if (sName.length() > MINCHAR && sName.length() < MAXCHAR_50) {
+            this.sName = sName;
+            bExito = true;
+        }
+        return bExito;
+    }
+}
--- ProyectoCienciapp/src/model/LimitsDB.java
+++ ProyectoCienciapp/src/model/LimitsDB.java
@@ -1,14 +1,15 @@
 package model;
 
 public interface LimitsDB {
+    public final byte MAXCHAR_30 = 30;
     public final byte MAXCHAR_50 = 50;
+    public final byte MAXCHAR_60 = 60;
+    public final byte MAXCHAR_65 = 65;
+    public final byte MAXCHAR_70 = 70;
+    public final byte MAXCHAR_100 = 100;
     public final short MAXCHAR_200 = 200;
     public final short MAXCHAR_250 = 250;
 
-    public final byte MAXCHAR_70 = 70;
-    public final byte MAXCHAR_60 = 60;
-    public final byte MAXCHAR_65 = 65;
-    public final byte MAXCHAR_100 = 100;
 
     public final byte MINCHAR = 0;
 
