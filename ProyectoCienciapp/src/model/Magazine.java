6be3b7bf718a7a8db634181b7d9ec81f9c58efda
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-06 14:45:55 +0200

    Creada la clase Magazine
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

---
+++ ProyectoCienciapp/src/model/Magazine.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Magazine implements LimitsDB {
+    private String sName;
+    private Headquarters oHeadquarters;
+
+    public String getsName() {
+        return sName;
+    }
+
+    private boolean setsName(String sName) {
+        boolean bExito = false;
+        if (sName != null && sName.length() < MAXCHAR_100 && sName.length() > MINCHAR) {
+            this.sName = sName;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+    public Headquarters getoHeadquarters() {
+        return oHeadquarters;
+    }
+
+    public boolean setoHeadquarters(Headquarters oHeadquarters) {
+        boolean bExito = false;
+        if (oHeadquarters.checkHeadquarters()) {
+            this.oHeadquarters = oHeadquarters;
+            bExito = true;
+        }
+        return bExito;
+    }
+
+
+}
