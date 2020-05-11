0f9c91ca374320f666ad79d02efd6c415189d64f
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-11 10:20:43 +0200

    Objeto UniversityController añadido a la clase PackageController
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/controller/Ctrl/PackageController.java
+++ ProyectoCienciapp/src/controller/Ctrl/PackageController.java
@@ -2,8 +2,10 @@ package controller.Ctrl;
 
 public class PackageController {
     private CountryController countryController;
+    private UniversityController universityController;
 
     public PackageController() {
+        universityController = new UniversityController();
         countryController = new CountryController();
     }
 
