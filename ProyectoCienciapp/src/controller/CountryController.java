831fa7ca078eeefb3af8f0a4367bd33c3d6308e3
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-11 10:09:37 +0200

    Creada la clase PackageController , CountryController y ICountryController
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

---
+++ ProyectoCienciapp/src/controller/CountryController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import model.Country;
+
+public class CountryController implements ICountryController {
+
+    // #########
+    // # CRUDS #
+    // #########
+    @Override
+    public int add(Country oObject) {
+        int iRes = 0;
+        if (oObject.checkCountry()) {
+            String sql = "INSERT INTO country VALUES (\"" + oObject.getsName() + "\")";
+            iRes = ConexionDB.executeUpdate(sql);
+        }
+        return iRes;
+    }
+
+    @Override
+    public int remove(Country oCounty) {
+        int iRes = 0;
+        String sql = "DELETE FROM country WHERE name LIKE \"" + oCounty.getsName() + "\"";
+        iRes = ConexionDB.executeUpdate(sql);
+        return iRes;
+    }
+
+    // ###########
+    // # QUERIES #
+    // ###########
+    @Override
+    public int existePais(Country oCountry) {
+        int iRes = 0;
+        if (oCountry.checkCountry()) {
+            String sql = "SELECT COUNT(*) FROM pais WHERE nombre LIKE \"" + oCountry.getsName() + "\"";
+            iRes = ConexionDB.executeCount(sql);
+        }
+        return iRes;
+    }
+}
---
+++ ProyectoCienciapp/src/controller/ICountryController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.Country;
+
+public interface ICountryController {
+    // #########
+    // # CRUDS #
+    // #########
+    int add(Country oObject);
+
+    int remove(Country oCounty);
+
+    // ###########
+    // # QUERIES #
+    // ###########
+    int existePais(Country oCountry);
+}
