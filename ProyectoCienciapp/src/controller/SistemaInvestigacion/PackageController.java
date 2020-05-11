8afbfd87bfb8e39ebdb2ae6e3f5fe92859013707
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-11 14:06:40 +0200

    Metido el controlador de la especializacion en el PackageController
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/controller/SistemaInvestigacion/PackageController.java
+++ ProyectoCienciapp/src/controller/SistemaInvestigacion/PackageController.java
@@ -6,11 +6,13 @@ public class PackageController {
     private UniversityController universityController;
     private FacultyController facultyController;
     private DepartmentController departmentController;
+    private SpecializationController specializationController;
 
     public PackageController() {
         universityController = new UniversityController();
         facultyController = new FacultyController();
         departmentController = new DepartmentController();
+        specializationController = new SpecializationController();
     }
 
 }
