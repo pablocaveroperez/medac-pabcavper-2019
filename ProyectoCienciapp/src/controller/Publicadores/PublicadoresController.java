915f7e91cddadc0b289457bfd37b25f395dd96df
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-11 14:24:40 +0200

    Getters de la clase SistemaInvestigacionController
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

---
+++ ProyectoCienciapp/src/controller/Publicadores/PublicadoresController.java
@@ -0,0 +1,4 @@
+package controller.Publicadores;
+
+public class PublicadoresController {
+}
--- ProyectoCienciapp/src/controller/SistemaInvestigacion/SistemaInvestigacionController.java
+++ ProyectoCienciapp/src/controller/SistemaInvestigacion/SistemaInvestigacionController.java
@@ -1,18 +1,40 @@
 package controller.SistemaInvestigacion;
 
-public class PackageController {
+
+public class SistemaInvestigacionController {
     private UniversityController universityController;
     private FacultyController facultyController;
     private DepartmentController departmentController;
     private SpecializationController specializationController;
     private MemberController memberController;
 
-    public PackageController() {
+    public SistemaInvestigacionController() {
         universityController = new UniversityController();
         facultyController = new FacultyController();
         departmentController = new DepartmentController();
         specializationController = new SpecializationController();
         memberController = new MemberController();
     }
+
+    public UniversityController getUniversityController() {
+        return universityController;
+    }
+
+    public FacultyController getFacultyController() {
+        return facultyController;
+    }
+
+    public DepartmentController getDepartmentController() {
+        return departmentController;
+    }
+
+    public SpecializationController getSpecializationController() {
+        return specializationController;
+    }
+
+    public MemberController getMemberController() {
+        return memberController;
+    }
+
 
 }
