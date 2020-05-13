e46da1e4c739c8d57bfae4a4b3c29ec562bb630f
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-13 21:21:45 +0200

    Terminado el metodo FacultyView.alta y comenzados los metodos FacultyView.busqueda y FacultyView.eliminar
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/SistemaInvestigacion/FacultyView.java
+++ ProyectoCienciapp/src/view/SistemaInvestigacion/FacultyView.java
@@ -1,9 +1,12 @@
 package view.SistemaInvestigacion;
 
 import controller.GeneralController;
+import model.LimitsDB;
+import model.SistemaInvestigacion.Faculty;
+import model.SistemaInvestigacion.University;
 import validaciones.ValidaLibrary;
 
-public class FacultyView {
+public class FacultyView implements LimitsDB {
     public static void menuFacultades(GeneralController controller) {
         byte bOpcion = 0;
         do {
@@ -28,6 +31,46 @@ public class FacultyView {
                     System.out.println("Volviendo...");
             }
         } while (bOpcion != 4);
+    }
+
+    private static int alta(GeneralController controller) {
+        String sFaculName = null;
+        String sUniName = null;
+        boolean errorControl = true;
+        Faculty oFaculty;
+
+        while (errorControl) {
+            try {
+                sFaculName = ValidaLibrary.leer("Introduce el nombre de la facultad: ");
+                if (sFaculName.length() > MINCHAR && sFaculName.length() <= MAXCHAR_50)
+                    errorControl = false;
+            }catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        errorControl = true;
+        while (errorControl) {
+            try {
+                sUniName = ValidaLibrary.leer("Introduce el nombre de la universidad a la que pertenece: ");
+                if (sUniName.length() <= MAXCHAR_65 && sUniName.length() > MINCHAR)
+                    errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        oFaculty = new Faculty(sFaculName, new University(sUniName));
+
+        return controller.addFaculty(oFaculty);
+    }
+
+    private static int eliminar(GeneralController controller) {
+        return 0;
+    }
+
+    private static void busqueda(GeneralController controller) {
+
     }
 
     private static byte opcionMenu() {
