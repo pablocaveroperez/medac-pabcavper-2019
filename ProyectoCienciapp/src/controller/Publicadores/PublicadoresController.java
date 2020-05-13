87b83d6812a6031e4de6915497f04128c0c1219e
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-13 13:32:07 +0200

    Metodos add remove y existe de la clase PublicadoresController
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/controller/Publicadores/PublicadoresController.java
+++ ProyectoCienciapp/src/controller/Publicadores/PublicadoresController.java
@@ -1,4 +1,48 @@
 package controller.Publicadores;
 
+import model.Publicadores.Headquarters;
+import model.Publicadores.Magazine;
+
 public class PublicadoresController {
+    private HeadquartersController headquartersController;
+    private MagazineController magazineController;
+
+    public PublicadoresController() {
+        headquartersController = new HeadquartersController();
+        magazineController = new MagazineController();
+    }
+
+    public HeadquartersController getHeadquartersController() {
+        return headquartersController;
+    }
+
+    public MagazineController getMagazineController() {
+        return magazineController;
+    }
+
+    // OFICINA PRINCIPAL
+    public int addHeadquarters(Headquarters oHeadquarters) {
+        return headquartersController.add(oHeadquarters);
+    }
+
+    public int removeHeadquarters(Headquarters oHeadquartes) {
+        return headquartersController.remove(oHeadquartes);
+    }
+
+    public int existeHeadquarters(Headquarters oHeadquarters) {
+        return headquartersController.existeHQ(oHeadquarters);
+    }
+
+    // REVISTA
+    public int addMagazine(Magazine oMagazine) {
+        return magazineController.add(oMagazine);
+    }
+
+    public int removeMagazine(Magazine oMagazine) {
+        return magazineController.remove(oMagazine);
+    }
+
+    public int existeMagazine(Magazine oMagazine) {
+        return magazineController.existeHQ(oMagazine);
+    }
 }
