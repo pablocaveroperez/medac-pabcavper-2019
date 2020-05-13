a3ad77f75f60111f85f3dfda104d8c745730916c
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-13 21:45:57 +0200

    Terminado el metodo alta de la clase DepartmentView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/SistemaInvestigacion/DepartmentView.java
+++ ProyectoCienciapp/src/view/SistemaInvestigacion/DepartmentView.java
@@ -1,9 +1,111 @@
 package view.SistemaInvestigacion;
 
 import controller.GeneralController;
+import model.LimitsDB;
+import model.SistemaInvestigacion.Department;
+import model.SistemaInvestigacion.Faculty;
+import validaciones.ValidaLibrary;
 
-public class DepartmentView {
+public class DepartmentView implements LimitsDB {
     public static void menuDepartamentos(GeneralController controller) {
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 4);
+    }
 
+    private static void busqueda(GeneralController controller) {
+
+    }
+
+    private static int eliminar(GeneralController controller) {
+        return 0;
+    }
+
+    private static int alta(GeneralController controller) {
+        byte idDepartment = 0;
+        String sName = null;
+        String sFaculName = null;
+        boolean errorControl = true;
+        Department oDepartment = null;
+
+        while (errorControl) {
+            try {
+                idDepartment = (byte) ValidaLibrary.valida("Introduce el ID del departamento: ", MINCHAR, MAXCHAR_30, 3);
+                errorControl = false;
+            }catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        errorControl = true;
+        while (errorControl) {
+            try {
+                sFaculName = ValidaLibrary.leer("Introduce el nombre de la facultad: ");
+                if (sFaculName.length() > MINCHAR && sFaculName.length() <= MAXCHAR_50)
+                    errorControl = false;
+            }catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        errorControl = true;
+        while (errorControl) {
+            try {
+                sName = ValidaLibrary.leer("Introduce el nombre del departamento: ");
+                if (sName == null || (sName.length() > MINCHAR && sName.length() < MAXCHAR_50))
+                    errorControl = false;
+            }catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        if (sName == null) {
+            oDepartment = new Department(idDepartment, new Faculty(sFaculName));
+        }else {
+            oDepartment = new Department(idDepartment, new Faculty(sFaculName),sName);
+        }
+
+        return controller.addDepartment(oDepartment);
+    }
+
+    private static byte opcionMenu() {
+        byte bOpcion = 0;
+        boolean errorControl = true;
+
+        System.out.println("\n\nFACULTADES: ");
+        System.out.println("1. Anadir.");
+        System.out.println("2. Eliminar.");
+        System.out.println("3. Busqueda.");
+        System.out.println("4. Volver.");
+
+        while (errorControl) {
+            try {
+                bOpcion = (byte) ValidaLibrary.valida("Introduce una opcion: ", 1, 4, 3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+        return bOpcion;
     }
 }
