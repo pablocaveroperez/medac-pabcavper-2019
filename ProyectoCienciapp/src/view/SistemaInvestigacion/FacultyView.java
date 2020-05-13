384b5d6faa291ef578fecb185b14ca7d0e0603f7
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-13 21:10:28 +0200

    Metodos FacultyView.menuFacultades y FacultyView.opcionMenu
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/SistemaInvestigacion/FacultyView.java
+++ ProyectoCienciapp/src/view/SistemaInvestigacion/FacultyView.java
@@ -1,9 +1,53 @@
 package view.SistemaInvestigacion;
 
 import controller.GeneralController;
+import validaciones.ValidaLibrary;
 
 public class FacultyView {
     public static void menuFacultades(GeneralController controller) {
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 4);
+    }
 
+    private static byte opcionMenu() {
+        byte bOpcion = 0;
+        boolean errorControl = true;
+
+        System.out.println("\n\nFACULTADES: ");
+        System.out.println("1. Anadir.");
+        System.out.println("2. Eliminar.");
+        System.out.println("3. Busqueda.");
+        System.out.println("4. Volver.");
+
+        while (errorControl) {
+            try {
+                bOpcion = (byte) ValidaLibrary.valida("Introduce una opcion: ", 1, 4, 3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+        return bOpcion;
     }
 }
