2708b8b1647dc1daf4151d758a907e36a95b96d3
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-14 12:16:25 +0200

    Metodo principal de la Clase ResearchTeamView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/SistemaInvestigacion/ResearchTeamView.java
+++ ProyectoCienciapp/src/view/SistemaInvestigacion/ResearchTeamView.java
@@ -4,6 +4,28 @@ import controller.GeneralController;
 
 public class ResearchTeamView {
     public static void menuEquiposInvestigacion(GeneralController controller) {
-
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 4);
     }
 }
