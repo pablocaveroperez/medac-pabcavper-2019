bf01be4ac07ab35feba542205d590d33e5b666f4
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-17 12:48:08 +0200

    Terminado el metodo alta de la clase MagazineView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/Publicaciones/MagazineView.java
+++ ProyectoCienciapp/src/view/Publicaciones/MagazineView.java
@@ -38,6 +38,37 @@ public class MagazineView implements LimitsDB {
         } while (bOpcion != 5);
     }
 
+    private static int alta(GeneralController controller) {
+        String sName = null;
+        String sHQ = null;
+        boolean errorControl = true;
+        Magazine oMagazine = null;
+
+        while (errorControl) {
+            try {
+                sName = ValidaLibrary.leer("Introduce el nombre de la revista: ");
+                if (sName.length() > MINCHAR && sName.length() < MAXCHAR_100)
+                    errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        errorControl = true;
+        while (errorControl) {
+            try {
+                sHQ = ValidaLibrary.leer("Introduce el nombre del HQ al que pertenece: ");
+                if (sHQ.length() > MINCHAR && sHQ.length() < MAXCHAR_100)
+                    errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        oMagazine = new Magazine(sName, new Headquarters(sHQ));
+        return controller.addMagazine(oMagazine);
+    }
+
     private static int eliminar(GeneralController controller) {
         String sName = null;
         boolean errorControl = true;
