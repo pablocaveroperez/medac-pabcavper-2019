f0be6ddd836b18118f34e6300542614f8c7af60e
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-17 17:41:46 +0200

    Terminado el metodo opcionMenu de la clase ArticleXCategoryView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/ArticulosUsuarios/ArticleXCategoryView.java
+++ ProyectoCienciapp/src/view/ArticulosUsuarios/ArticleXCategoryView.java
@@ -1,8 +1,15 @@
 package view.ArticulosUsuarios;
 
 import controller.GeneralController;
+import model.ArticulosUsuarios.Article;
+import model.ArticulosUsuarios.ArticleType;
+import model.ArticulosUsuarios.ArticleXCategory;
 import model.LimitsDB;
+import model.Publicadores.Magazine;
+import model.SistemaInvestigacion.Member;
 import validaciones.ValidaLibrary;
+
+import java.util.List;
 
 public class ArticleXCategoryView implements LimitsDB {
     public static void menuArticleXCategory(GeneralController controller) {
@@ -32,6 +39,93 @@ public class ArticleXCategoryView implements LimitsDB {
                     System.out.println("Volviendo...");
             }
         } while (bOpcion != 5);
+    }
+
+    private static int alta(GeneralController controller) {
+        byte idArticle = 0;
+        String sCategoryName = null;
+        ArticleXCategory oArticleXCategory = null;
+        boolean errorControl = true;
+
+        while (errorControl) {
+            try {
+                idArticle = (byte) ValidaLibrary.valida("Introduce el ID del articulo: ",MINCHAR,MAXCHAR_100,3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        errorControl = true;
+        while (errorControl) {
+            try {
+                sCategoryName = ValidaLibrary.leer("Introduce la categoria: ");
+                if (sCategoryName.length() > MINCHAR && sCategoryName.length() < MAXCHAR_60)
+                    errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        oArticleXCategory = new ArticleXCategory();
+
+        return controller.addArticle(oArticleXCategory);
+    }
+
+    private static int eliminar(GeneralController controller) {
+        byte idArticle = 0;
+        Article oArticle = null;
+        boolean errorControl = true;
+
+        while (errorControl) {
+            try {
+                idArticle = (byte) ValidaLibrary.valida("Introduce el ID del articulo: ",MINCHAR,MAXCHAR_100,3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        oArticle = new Article(idArticle);
+        return controller.removeArticle(oArticle);
+    }
+
+    private static void busqueda(GeneralController controller) {
+        byte idArticle = 0;
+        Article oArticle = null;
+        boolean errorControl = true;
+        int iNumArticles;
+
+        while (errorControl) {
+            try {
+                idArticle = (byte) ValidaLibrary.valida("Introduce el ID del articulo: ",MINCHAR,MAXCHAR_100,3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+
+        oArticle = new Article(idArticle);
+        iNumArticles = controller.existeArticle(oArticle);
+
+        if (iNumArticles > 0) {
+            System.out.println("Existen " + iNumArticles + " en la base de datos.");
+        }else
+            System.out.println("No existe ningun articulo con ese ID.");
+    }
+
+    private static void mostrarTodas(GeneralController controller) {
+        List<Article> lArticle;
+
+        lArticle = controller.getArticulosUsuariosController().getArticleController().getTodasArticulo();
+
+        if (lArticle.size() > 0) {
+            for (Article oArticle : lArticle) {
+                System.out.println(oArticle);
+            }
+        }else{
+            System.out.println("No hay ningun articulo en la base de datos.");
+        }
     }
 
     private static byte opcionMenu() {
