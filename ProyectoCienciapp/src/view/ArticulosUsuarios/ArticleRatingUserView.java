6dc1c2a6bf2c630950508c94fb86bcaf2a215d66
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-17 17:56:21 +0200

    Creada el metodo menu de la clase ArticleRatingUserView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/ArticulosUsuarios/ArticleRatingUserView.java
+++ ProyectoCienciapp/src/view/ArticulosUsuarios/ArticleRatingUserView.java
@@ -1,6 +1,35 @@
 package view.ArticulosUsuarios;
 
+import controller.GeneralController;
 import model.LimitsDB;
 
 public class ArticleRatingUserView implements LimitsDB {
+    public static void menuArticleXCategory(GeneralController controller) {
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                case 4:
+                    mostrarTodas(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 5);
+    }
 }
