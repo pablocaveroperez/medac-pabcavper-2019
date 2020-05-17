a4eb0d09bc16df9f24dcee74db9d45127a0a08be
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-17 16:41:03 +0200

    Creada la clase ArticleTypeView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

---
+++ ProyectoCienciapp/src/view/ArticulosUsuarios/ArticleTypeView.java
@@ -0,0 +1,58 @@
+package view.ArticulosUsuarios;
+
+import controller.GeneralController;
+import model.LimitsDB;
+import validaciones.ValidaLibrary;
+
+public class ArticleTypeView implements LimitsDB {
+    public static void menuArticleType(GeneralController controller) {
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                case 4:
+                    mostrarTodas(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 5);
+    }
+
+    private static byte opcionMenu() {
+        byte bOpcion = 0;
+        boolean errorControl = true;
+
+        System.out.println("\n\nTIPO DE ARTICULO: ");
+        System.out.println("1. Anadir.");
+        System.out.println("2. Eliminar.");
+        System.out.println("3. Busqueda.");
+        System.out.println("4. Mostrar todas.");
+        System.out.println("5. Volver.");
+
+        while (errorControl) {
+            try {
+                bOpcion = (byte) ValidaLibrary.valida("Introduce una opcion: ", 1, 5, 3);
+                errorControl = false;
+            } catch (Exception exception) {
+                System.out.println("Error: " + exception.getMessage());
+            }
+        }
+        return bOpcion;
+    }
+}
