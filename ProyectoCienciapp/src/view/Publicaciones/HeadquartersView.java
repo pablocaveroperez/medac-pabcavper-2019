4ae6472b35d3da6134c6d4d1aed0dbb7c836fe06
pcavero99 <34574208+pcavero99@users.noreply.github.com>
2020-05-17 11:55:54 +0200

    creado el metodo menuHeadquarters en la clase HeadquartersView
    Signed-off-by: pcavero99 <34574208+pcavero99@users.noreply.github.com>

--- ProyectoCienciapp/src/view/Publicaciones/HeadquartersView.java
+++ ProyectoCienciapp/src/view/Publicaciones/HeadquartersView.java
@@ -5,6 +5,31 @@ import model.LimitsDB;
 
 public class HeadquartersView implements LimitsDB {
     public static void menuHeadquarters(GeneralController controller) {
-
+        byte bOpcion = 0;
+        do {
+            bOpcion = opcionMenu();
+            switch (bOpcion) {
+                case 1:
+                    if (alta(controller) != 0)
+                        System.out.println("Ha sido anadida con exito.");
+                    else
+                        System.out.println("Ya se encuentra en el sistema.");
+                    break;
+                case 2:
+                    if (eliminar(controller) != 0)
+                        System.out.println("Ha sido eliminado con existo.");
+                    else
+                        System.out.println("No existe en el sistema.");
+                    break;
+                case 3:
+                    busqueda(controller);
+                    break;
+                case 4:
+                    mostrarTodas(controller);
+                    break;
+                default:
+                    System.out.println("Volviendo...");
+            }
+        } while (bOpcion != 5);
     }
 }
